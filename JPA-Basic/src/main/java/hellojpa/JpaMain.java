a2cd0ad4ac3f6d4579a1d5bdaa660d3966096440
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-17 15:52:18 +0900

    플러시

--- JPA-Basic/src/main/java/hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -14,9 +14,11 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
-            
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+
+            em.flush();
+
             System.out.println("====================");
             tx.commit();
         } catch (Exception e) {
