bc75f334751302184bc2414d1d69c1616f3c14c3
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-17 13:38:41 +0900

    Hello JPA - 애플리케이션 개발

---
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import hellojpa.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
