13bfab13f8f0d1b68bb4cd61de6ed9303e048743
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-17 16:07:36 +0900

    준영속

--- JPA-Basic/src/main/java/hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -14,10 +14,13 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            em.flush();
+            //em.detach(member);
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("====================");
             tx.commit();
