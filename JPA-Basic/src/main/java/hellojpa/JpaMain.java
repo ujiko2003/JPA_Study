e9ba7f526b60188d7f1346ef8930f5f983b28b33
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-18 11:44:08 +0900

    기본 키 매핑

--- JPA-Basic/src/main/java/hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -1,7 +1,6 @@
 package hellojpa;
 
 import hellojpa.entity.Member;
-import hellojpa.value.RoleType;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -10,47 +9,39 @@ import javax.persistence.Persistence;
 
 public class JpaMain {
 
-    /**
-     * hbm2ddl.auto 어노테이션
-     * - @Column : 컬럼매핑
-     * - @Temporal : 날짜 타입 매핑
-     * - @Enumerated : enum 타입 매핑
-     * - @Lob : BLOB, CLOB 매핑
-     * - @Transient : 특정 필드를 컬럼에 매핑하지 않음
-     */
-
-    /**
-     * Column 어노테이션
-     * - name : 필드와 매핑할 테이블의 컬럼 이름 (default : 객체 필드 이름)
-     * - insertable, updatable : 등록, 변경 가능 여부 설정 (default : true)
-     * - nullable : null 값의 허용 여부 설정 (default : true)
-     * - unique : 유니크 제약 조건 설정 (default : false)
-     * - columnDefinition : 데이터베이스 컬럼 정보를 직접 설정
-     * - length : 문자 길이 제약 조건 설정 (String 타입에만 사용) (default : 255)
-     */
-
-    /**
-     * Enumerated 어노테이션
-     * - EnumType.ORDINAL : enum 순서를 DB에 저장 (default)
-     * - EnumType.STRING : enum 이름을 DB에 저장
-     * - 주로 EnumType.STRING을 사용
-     * - ORDINAL은 enum 순서가 바뀌면 데이터가 꼬일 수 있음
-     * - EnumType.STRING은 enum 이름이 바뀌어도 데이터가 꼬이지 않음
-     */
-
     public static void main(String[] args) {
+
+        /*
+         * 기본 키 매핑 방법
+         * - 직접 할당 : @Id만 사용
+         * - 자동 생성 : @GeneratedValue
+         *   - IDENTITY: 데이터베이스에 위임 (예: MySQL : AUTO_INCREMENT)
+         *   - SEQUENCE: 데이터베이스 시퀀스 오브젝트 사용 (예: Oracle)
+         *   - TABLE: 키 생성 테이블 사용 (모든 데이터베이스에 사용 가능, 성능이슈로 잘 사용하지 않음)
+         *   - AUTO: 방언에 따라 자동 지정, 기본값
+         */
+
+        /*
+         * IDENTITY 전략
+         * - 주로 MySQL, PostgreSQL, SQL Server, DB2에서 사용
+         * - JPA는 보통 트랜잭션 커밋 시점에 INSERT SQL 실행
+         * - AUTO_INCREMENT는 데이터베이스에 INSERT SQL을 실행한 이후에 ID 값을 알 수 있음
+         * - IDENTITY 전략은 em.persist() 시점에 즉시 INSERT SQL을 실행하고 DB에서 식별자를 조회
+         */
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
+
         try {
-
             Member member = new Member();
-            member.setId(3L);
-            member.setUsername("C");
-            member.setRoleType(RoleType.GUEST);
+            member.setName("D");
 
+            System.out.println("==================");
             em.persist(member);
+            System.out.println("member.Id = " + member.getId());
+            System.out.println("==================");
 
             tx.commit();
         } catch (Exception e) {
--- JPA-Basic/src/main/java/hellojpa/entity/Member.java
+++ JPA-Basic/src/main/java/hellojpa/entity/Member.java
@@ -1,48 +1,19 @@
 package hellojpa.entity;
 
-import hellojpa.value.RoleType;
-
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
-@Table(name = "MBR")
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
-    private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate;
-
-    private LocalDateTime testLocalDateTime;
-
-    @Lob
-    private String description;
-
-    @Transient // DB에 저장되지 않는 필드, 메모리에서만 사용
-    private int temp;
+    @Column(name = "name", nullable = false)
+    private String name;
 
     public Member() {
     }
-
-    // Getter, Setter
-
 
     public Long getId() {
         return id;
@@ -52,59 +23,11 @@ public class Member {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
+    public void setName(String name) {
+        this.name = name;
     }
 }
