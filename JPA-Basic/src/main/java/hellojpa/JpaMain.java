a216559faecd8528e88fa21a3ef2dd7f79b52151
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-19 15:49:39 +0900

    영속성 전이(CASCADE)와 고아 객체

--- JPA-Basic/src/main/java/hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -1,38 +1,60 @@
 package hellojpa;
 
-import hellojpa.entity.Member;
-import hellojpa.entity.Team;
+import hellojpa.entity.Child;
+import hellojpa.entity.Parent;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
     /*
-     * 프록시와 즉시로딩 주의
-     * - 가급적 지연 로딩만 사용 (특히 실무에서)
-     * - 즉시 로딩을 적용하면 예상하지 못한 SQL이 발생
-     * - 즉시 로딩은 JPQL에서 N+1 문제를 일으킨다.
-     * - @ManyToOne, @OneToOne은 기본이 즉시 로딩 -> LAZY로 설정
-     * - @OneToMany, @ManyToMany는 기본이 지연 로딩
+     * 영속성 전이: CASCADE
+     * - 특정 엔티티를 영속 상태로 만들 때 연관된 엔티티도 함께 영속상태로 만들고 싶을 때
+     * - 예: 부모 엔티티를 저장할 때 자식 엔티티도 함께 저장.
      */
 
     /*
-     * 지연 로딩 활용
-     * - Member와 Team은 자주 함께 사용 -> 즉시 로딩
-     * - Member와 Order는 가끔 사용 -> 지연 로딩
-     * - Order와 Product는 자주 함께 사용 -> 즉시 로딩
+     * 영속성 전이: CASCADE - 주의!
+     * - 영속성 전이는 연관관계를 매핑하는 것과 아무 관련이 없음
+     * - 엔티티를 영속화할 때 연관된 엔티티도 함께 영속화하는 편리함을 제공할 뿐
      */
 
     /*
-     * 지연 로딩 활용 - 실무
-     * - 모든 연관관계에 지연 로딩을 사용해라!
-     * - 실무에서 즉시 로딩을 사용하지 마라!
-     * - JPQL fetch 조인이나, 엔티티 그래프 기능을 사용해라!
-     * - 즉시 로딩을 적용하면 예상하지 못한 SQL이 발생한다.
+     * CASCADE의 종류
+     * - ALL: 모두 적용
+     * - PERSIST: 영속
+     * - REMOVE: 삭제
+     * - MERGE: 병합
+     * - REFRESH: REFRESH
+     * - DETACH: DETACH
+     */
+
+    /*
+     * 고아 객체
+     * - 고아 객체 제거: 부모 엔티티와 연관관계가 끊어진 자식 엔티티를 자동으로 삭제
+     * - orphanRemoval = true
+     */
+
+    /*
+     * 고아 객체 - 주의
+     * - 참조가 제거된 엔티티는 다른 곳에서 참조하지 않는 고아 객체로 보고 삭제하는 기능
+     * - 참조하는 곳이 하나일 때 사용해야함!
+     * - 특정 엔티티가 개인 소유할 때 사용
+     * - @OneToOne, @OneToMany만 가능
+     * - 참고: 개념적으로 부모를 제거하면 자식은 고아가 된다. 따라서
+     *       고아 객체 제거 지능을 활성화 하면, 부모를 제거할 때 자식도 함께
+     *       제거된다. 이것은 CascadeType.REMOVE 처럼 동작한다.
+     */
+
+    /*
+     * 영속성 전이 + 고아 객체, 생명주기
+     * - CascadeType.ALL + orphanRemoval=true
+     * - 스스로 생명주기를 관리하는 엔티티는 em.persist()로 영속화, em.remove()로 제거
+     * - 두 옵션을 모두 활성화 하면 부모 엔티티를 통해서 자식의 생명주기를 관리할 수 있음
+     * - 도메인 주도 설계(DDD)의 Aggregate Root 개념을 구현할 때 유용
      */
 
     public static void main(String[] args) {
@@ -43,31 +65,23 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
 
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUserName("member1");
-            member1.setTeam(team);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member2 = new Member();
-            member2.setUserName("member2");
-            member2.setTeam(team2);
-            em.persist(member2);
+            em.persist(parent);
+            em.persist(child1);
+            em.persist(child2);
 
             em.flush();
             em.clear();
 
-//            Member member = em.find(Member.class, member1.getId());
-            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class).getResultList();
-            members.get(0).getTeam();
-
+            Parent findParent = em.find(Parent.class, parent.getId());
+            em.remove(findParent);
 
             tx.commit();
         } catch (Exception e) {
