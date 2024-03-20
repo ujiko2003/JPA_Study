9ba93fcae45dba8b55c981e29f12498652cf158b
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-20 15:14:46 +0900

    값 타입 컬렉션

--- JPA-Basic/src/main/java/hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import hellojpa.entity.Address;
+import hellojpa.entity.AddressEntity;
 import hellojpa.entity.Member;
 
 import javax.persistence.EntityManager;
@@ -10,6 +11,29 @@ import javax.persistence.Persistence;
 
 public class JpaMain {
 
+    /*
+     * 값 타입컬렉션의 제약사항
+     *  - 값 타입은 엔티티와 다르게 식별자 개념이 없다.
+     *  - 값은 변경하면 추적이 어렵다.
+     *  - 값 타입 컬렉션에 변경사항이 발생하면, 주인 엔티티와 연관된 모든 데이터를 삭제하고, 값 타입 컬렉션에 있는 현재값을 모두 다시 저장한다.
+     *  - 따라서 값 타입 컬렉션에 변경사항이 발생하면, 추적이 어렵고, 비용이 많이 든다.
+     */
+
+    /*
+     * 정리
+     * 1. 엔티티 타입의 특징
+     *  - 식별자 O
+     *  - 생명주기 관리 O
+     *  - 공유 O
+     *
+     * 2. 값 타입의 특징
+     *  - 식별자 X
+     *  - 생명주기를 엔티티에 의존
+     *  - 공유하지 않는 것이 안전
+     *  - 불변 객체로 만드는 것이 안전
+     *
+     */
+
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -18,20 +42,34 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Member member = new Member();
+            member.setUserName("member1");
+            member.setHomeAddress(new Address("HomeCity", "street", "zipcode"));
 
-            Address address = new Address("city", "street", "10000");
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
 
-            Member member1 = new Member();
-            member1.setUserName("member1");
-            member1.setHomeAddress(address);
-            em.persist(member1);
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "zipcode"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "zipcode"));
 
-            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+            em.persist(member);
 
-            Member member2 = new Member();
-            member2.setUserName("member2");
-            member2.setHomeAddress(copyAddress);
-            em.persist(member2);
+            em.flush();
+            em.clear();
+
+            System.out.println("============== START ==============");
+            Member findMember = em.find(Member.class, member.getId());
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", "street", "zipcode"));
+
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+
+            System.out.println("============== END ==============");
 
             tx.commit();
         } catch (Exception e) {
--- JPA-Basic/src/main/java/hellojpa/entity/Member.java
+++ JPA-Basic/src/main/java/hellojpa/entity/Member.java
@@ -1,6 +1,10 @@
 package hellojpa.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -19,13 +23,18 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
-    @Embedded
-    @AttributeOverrides({
-            @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
-            @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
-            @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
-    })
-    private Address workAddress;
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
 
     public Member() {
     }
@@ -60,5 +69,21 @@ public class Member {
 
     public void setHomeAddress(Address address) {
         this.homeAddress = address;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
     }
 }
