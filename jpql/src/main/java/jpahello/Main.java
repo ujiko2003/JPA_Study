7fafb2730a6c600a376390644355beac4c4bc0c8
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-20 18:43:43 +0900

    경로 표현식

--- jpql/src/main/java/jpahello/Main.java
+++ jpql/src/main/java/jpahello/Main.java
@@ -1,9 +1,32 @@
 package jpahello;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
+
+    /*
+     * 경로 표현식
+     *  - 상태 필드(state field): 단순히 값을 저장하기 위한 필드
+     *  - 연관 필드(association field): 연관관계를 위한 필드
+     *  - 단일 값 연관 필드: @ManyToOne, @OneToOne, 대상이 엔티티
+     */
+
+    /*
+     * 경로 표현식 특징
+     *  - 상태 필드(state field): 경로 탐색의 끝, 더 이상 탐색 X
+     *  - 단일 값 연관 경로: 묵시적 내부 조인 발생, 탐색 O
+     *  - 컬렉션 값 연관 경로: 묵시적 내부 조인 발생, 탐색 X
+     *   - FROM 절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색 가능
+     */
+
+    /*
+     * 경로 탐색을 사용한 묵시적 조인 시 주의사항
+     *  - 항상 내부 조인
+     *  - 컬렉션은 경로 탐색의 끝, 명시적 조인을 통해 별칭을 얻어야 함
+     *  - 경로 탐색은 주로 SELECT, WHERE 절에서 사용하지만 묵시적 조인으로 인해 SQL의 FROM (JOIN) 절에 영향을 줌
+     */
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
@@ -26,13 +49,12 @@ public class Main {
             em.flush();
             em.clear();
 
-            String query = "select  locate('de','abcdefg') FROM Member m";
-            List<Integer> result = em.createQuery(query, Integer.class)
+            String query = "select m.username FROM Team t join t.members m";
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
-            for (Integer s : result) {
-                System.out.println("s = " + s);
-            }
+            System.out.println("result = " + result);
+
 
             tx.commit();
         } catch (Exception e) {
