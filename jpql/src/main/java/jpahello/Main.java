e974366bb33753719dabcccd336f740902204422
HyeonJongKim <ujiko2003@bpsoft.co.kr>
2024-03-20 17:33:47 +0900

    조인

--- jpql/src/main/java/jpahello/Main.java
+++ jpql/src/main/java/jpahello/Main.java
@@ -12,28 +12,23 @@ public class Main {
 
         try {
 
-            for (int i=0; i<100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+            member.setTeam(team);
+            em.persist(member);
 
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc ", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            String query = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-
-            System.out.println("result.size = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
-
-
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
